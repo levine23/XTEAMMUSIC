ab8edcfffdacbd82847945f2aecc7a3f6933614f
teamx-cloner <150880343+xteam-cloner@users.noreply.github.com>
2025-05-03 08:38:32 +0700

    Update ping.py

--- ChampuMusic/plugins/tools/ping.py
+++ ChampuMusic/plugins/tools/ping.py
@@ -35,3 +35,31 @@ async def ping_com(client, message: Message, _):
         ),
         reply_markup=support_group_markup(_),
     )
+
+
+@app.on_message(filters.command(["tping", "talive"]) & ~BANNED_USERS)
+@language
+async def ping_com(client, message: Message, _):
+    response = await message.reply_photo(
+        photo=PING_IMG_URL,
+        caption=_["ping_1"].format(app.mention),
+    )
+    start = datetime.now()
+    pytgping = await Champu.ping()
+    UP, CPU, RAM, DISK = await bot_sys_stats()
+    resp = (datetime.now() - start).microseconds / 1000
+    effect_id = 5104841245755180586  # Menambahkan nilai effect_id secara langsung
+    await response.edit_text(
+        _["ping_2"].format(
+            resp,
+            app.mention,
+            UP,
+            RAM,
+            CPU,
+            DISK,
+            pytgping,
+            effect_id,  # Menambahkan effect_id ke dalam format
+        ),
+        reply_markup=support_group_markup(_),
+    )
+    
